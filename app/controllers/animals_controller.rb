5a1f56db1d63efbd1e84f8746c014afffe2b0135
Paolo Lenin Artigas Chacin <paoloartigas21@gmail.com>
2025-06-19 23:06:47 -0400

    CRUD

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,22 +1,58 @@
 class AnimalsController < ApplicationController
+  before_action :set_location
+  before_action :set_animal, only: [:show, :edit, :update, :destroy]
+
   def index
+    if params[:query].present?
+      @animals = @location.animals.where("alias ILIKE ?", "%#{params[:query]}%")
+    else
+      @animals = @location.animals
+    end
   end
 
   def show
   end
 
   def new
+    @animal = @location.animals.new
   end
 
   def create
+    @animal = @location.animals.new(animal_params)
+    if @animal.save
+      redirect_to location_animals_path(@location), notice: "Animal creado exitosamente."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @animal.update(animal_params)
+      redirect_to location_animals_path(@location), notice: "Animal actualizado exitosamente."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
-  def destoy
+  def destroy
+    @animal.destroy
+    redirect_to location_animals_path(@location), notice: "Animal eliminado."
+  end
+
+  private
+
+  def set_location
+    @location = Location.find(params[:location_id])
+  end
+
+  def set_animal
+    @animal = @location.animals.find(params[:id])
+  end
+
+  def animal_params
+    params.require(:animal).permit(:breed, :alias, :birth_date, :gender, :follow, :animal_group_id)
   end
 end
