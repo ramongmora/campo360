793810e43f524e2f4843aec8ebd1f5d7cd7e7128
Rodrigo Flores <floresrodrigodf@gmail.com>
2025-06-21 10:58:23 -0600

    Fix: se actualizan seeds con nombres reales

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,29 +32,46 @@ activity_names = [
 ]
 activities = activity_names.map { |name| Activity.create!(name: name) }
 
-puts "Creating 10 animal groups..."
-animal_group_names = [
-  "Cattle", "Horses", "Pigs", "Chickens", "Rabbits",
-  "Goats", "Sheep", "Turkeys", "Ducks", "Guinea Fowls"
-]
-animal_groups = animal_group_names.map { |name| AnimalGroup.create!(name: name) }
+puts "Creating animal groups..."
+animal_group_names = {
+  "Cattle" => ["Holstein", "Jersey", "Angus", "Hereford"],
+  "Horses" => ["Quarter Horse", "Arabian", "Thoroughbred", "Clydesdale"],
+  "Pigs" => ["Yorkshire", "Duroc", "Berkshire", "Hampshire"],
+  "Chickens" => ["Leghorn", "Rhode Island Red", "Plymouth Rock", "Silkie"],
+  "Rabbits" => ["Flemish Giant", "New Zealand", "Californian", "Dutch"],
+  "Goats" => ["Boer", "Nubian", "Alpine", "LaMancha"],
+  "Sheep" => ["Merino", "Suffolk", "Dorset", "Hampshire"],
+  "Turkeys" => ["Broad Breasted White", "Bourbon Red", "Narragansett", "Royal Palm"],
+  "Ducks" => ["Pekin", "Mallard", "Khaki Campbell", "Rouen"],
+  "Guinea Fowls" => ["Helmeted", "White", "Lavender", "Pearl"]
+}
+animal_groups = []
+
+animal_group_names.each do |group_name, _|
+  animal_groups << AnimalGroup.create!(name: group_name)
+end
 
 puts "Creating 5 employees per location..."
 Location.all.each do |location|
   5.times do
-  Employee.create!(
-    email: Faker::Internet.unique.email,
-    password: "password",
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    citizen_register: Faker::IdNumber.valid,
-    salary: rand(1000..2000),
-    location_id: location.id # 👈 cambio aquí
-)
+    Employee.create!(
+      email: Faker::Internet.unique.email,
+      password: "password",
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      citizen_register: Faker::IdNumber.valid,
+      salary: rand(1000..2000),
+      location: location
+    )
   end
 end
 
-puts "Creating 4 crops..."
+puts "Creating crops..."
+crop_types = [
+  "Wheat", "Corn", "Rice", "Soy", "Barley", "Oats", "Sorghum", "Sunflower",
+  "Alfalfa", "Rye", "Cotton", "Canola", "Peanuts", "Sugarcane", "Potatoes"
+]
+
 4.times do
   sowing = Faker::Date.between(from: 6.months.ago, to: Date.today)
   harvest = Faker::Date.between(from: sowing + 30, to: sowing + 180)
@@ -64,24 +81,25 @@ puts "Creating 4 crops..."
     sowing_date: sowing,
     harvest_date: harvest,
     surface: rand(0.5..10.0).round(2),
-    kind: %w[Wheat Corn Rice Soy].sample,
+    kind: crop_types.sample,
     follow: [true, false].sample
   )
 end
 
 puts "Creating 50 animals..."
 50.times do
+  group = animal_groups.sample
+  breed_options = animal_group_names[group.name]
   Animal.create!(
     alias: Faker::Creature::Animal.name,
-    breed: Faker::Creature::Animal.name,
+    breed: breed_options.sample,
     birth_date: Faker::Date.between(from: 2.years.ago, to: Date.today),
     gender: %w[male female].sample,
     follow: [true, false].sample,
     location: Location.all.sample,
-    animal_group: animal_groups.sample
+    animal_group: group
   )
 end
-
 
 puts "Creating 50 schedules (randomly assigned to crops or animals)..."
 50.times do
