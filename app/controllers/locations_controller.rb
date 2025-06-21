af8603dfbcfeccda09967d9cc381aa436ad6287f
CristobalNoriega <cristobal.noriegaa@gmail.com>
2025-06-21 13:37:04 -0400

    feat: se completo el controller de locations con vistas basicas

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,23 +1,50 @@
 class LocationsController < ApplicationController
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
+
   def index
-    @locations = Location.find_by(user_id: current_user)
+    @locations = Location.where(user_id: current_user.id)
   end
 
   def show
   end
 
   def new
+    @location = Location.new
   end
 
   def create
+    @location = Location.new(location_params)
+    @location.user = current_user
+    if @location.save
+      redirect_to @location, notice: 'Location was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @location.update(location_params)
+      redirect_to @location, notice: 'Location was successfully updated.'
+    else
+      render :edit
+    end
   end
 
-  def destoy
+  def destroy
+    @location.destroy
+    redirect_to locations_url, notice: 'Location was successfully destroyed.'
+  end
+
+  private
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(:name, :address, :latitude, :longitude)
   end
 end
