fad6372475870088b57e082b81d9dc4f4f07dead
Ramon Garcia <jramon.mora03@gmail.com>
2025-06-23 21:18:10 -0700

    feat: se arreglo la navbar, se creo devise para inciar sesion, se ajusto el front de inicio y registrar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  helper_method :current_location
+
+  private
+
+  def current_location
+    return nil unless user_signed_in?
+    current_user.locations.first
+  end
+
+  protected
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
