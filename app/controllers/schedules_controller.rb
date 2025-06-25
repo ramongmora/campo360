6704a3c4c621e121060caf9390d605d5c3f78546
Rodrigo Flores <floresrodrigodf@gmail.com>
2025-06-25 14:52:41 -0600

    Feat: se grega index a shedules

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,22 +1,38 @@
 class SchedulesController < ApplicationController
   def index
+    @schedules = Location.find(params[:location_id]).schedules
   end
 
   def show
+    # @schedulable = Schedule.find(params[:schedulable_id])
   end
 
   def new
+    # @schdedule = Schedule.new(schedule_params)
   end
 
   def create
+    # @schedule.create(schedule_params)
+
   end
 
   def edit
+    # @schedulable = Schedule.find(params[:schedulable_id])
+
   end
 
   def update
+    # @schdedulable Schedule.find(params[:schedulable_id])
+
   end
 
   def destroy
+    # Schedule.find(params[:schedulable_id])
+    # @schdulable.destroy {turbo_method, "delete"},
   end
+
+private
+# def schedule_params
+
+
 end
