e54e8bb816e5202c6aa3510e468837b65888b642
Ramon Garcia <jramon.mora03@gmail.com>
2025-07-05 09:33:40 -0700

    feat: se arreglo la landing el hero con boton de empezar ahora

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  # Permitir acceso público a la landing
-  skip_before_action :authenticate_user!, only: :home
+  # Permitir acceso público a la landing y soporte
+  skip_before_action :authenticate_user!, only: [:home, :support]
 
   def home
     @location = Location.first
@@ -19,10 +19,7 @@ class PagesController < ApplicationController
   def ajustes
     # Lógica opcional para ajustes de usuario
   end
-
-  class PagesController < ApplicationController
-    def support
-    end
+  def support
+    # Soporte público
   end
-
 end
